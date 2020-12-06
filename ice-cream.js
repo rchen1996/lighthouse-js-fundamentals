c826855d21aa262bcf5614d8001f6198ca02da03
Rebecca Chen <rebecca061996@gmail.com>
2020-12-06 00:25:07 +0000

    Using index to access values in an array and accessing the array's length property

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+//create an array of iceCreamFlavours
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// Add flavour "root beer" to array without modifying the original array. Then console.log entire array
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//console.log first flavour in array
+console.log(iceCreamFlavours[0]);
+
+// Get the Last Flavour - console.log
+console.log(iceCreamFlavours[5]);
+
+//console.log number of flavours in the array
+console.log(iceCreamFlavours.length);
