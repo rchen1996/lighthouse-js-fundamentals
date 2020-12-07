8f63d88aa5092dac5f5e65d17d35cc488dcefe5c
Rebecca Chen <rebecca061996@gmail.com>
2020-12-07 06:53:58 +0000

    Using a function to concatenate two arrays together

---
+++ concatenate-arrays.js
@@ -0,0 +1,11 @@
+// define a function such that when given 2 arrays, concatenate the arrays together
+function concat(array, arrayPlus) {
+  for (let i = 0; i < arrayPlus.length; i++) {
+    array.push(arrayPlus[i])
+  }
+  return array
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
